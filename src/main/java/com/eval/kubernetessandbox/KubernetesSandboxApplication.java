5bbdeac934acced521666519c8b334b046e1479e
Evgenii Aleksandrov <evgeniy.alexandrov1994@gmail.com>
2022-12-04 20:07:29 +0100

    initial commit

---
+++ src/main/java/com/eval/kubernetessandbox/KubernetesSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.eval.kubernetessandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eval/kubernetessandbox/KubernetesSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eval.kubernetessandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
